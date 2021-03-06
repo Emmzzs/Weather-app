797a66d545e04cc32ff6da26ef54160085a12a15
Emmzzs <78304688+Emmzzs@users.noreply.github.com>
2021-03-06 11:52:10 +0100

    Changed JS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-let now = new Date();
 
-function getTimeDay(currentDate) {
+function getTimeDay(timestamp) {
+  let now = new Date(timestamp);
+
   let hours = now.getHours();
 
   let day = now.getDay();
@@ -24,25 +25,70 @@ function getTimeDay(currentDate) {
   return `${today} ${hours}:${minutes}`;
 }
 
-function searchCity(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#inputCity");
-  let city = searchInput.value;
-  let forecastToday = document.querySelector("#Today");
-  forecastToday.innerHTML = city;
-  let apiKey = "8e1200888b2b1e98ddd0437072d3d7cf";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showWeather);
+function displayTemperature(response) {
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  celciusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celciusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let h2 = document.querySelector("h2");
-h2.innerHTML = getTimeDay(now);
+function dispalyForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
 
-let form = document.querySelector("#form_City");
-form.addEventListener("submit", searchCity);
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+    <div class="col-2">
+      <h3>
+        ${formatHours(forecast.dt * 1000)}
+      </h3>
+      <img
+        src="http://openweathermap.org/img/wn/${
+          forecast.weather[0].icon
+        }@2x.png"
+      />
+      <div class="weather-forecast-temperature">
+        <strong>
+          ${Math.round(forecast.main.temp_max)}°
+        </strong>
+        ${Math.round(forecast.main.temp_min)}°
+      </div>
+    </div>
+  `;
+  }
+}
 
-function findCurrentLocation(position) {
-  console.log(position);
+function searchCity(city) {
+  let apiKey = "8e1200888b2b1e98ddd0437072d3d7cf";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(dispalyForecast);
+}
+
+function getCurrentCity(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(findPosition);
 }
 
 function findPosition(position) {
@@ -52,22 +98,42 @@ function findPosition(position) {
 
   let apiKey = "d57548feb68ff97f5b47ea4b49ae289d";
   let apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${currentLat}&lon=${currentLong}&appid=${apiKey}&units=metric`;
-  axios.get(apiURL).then(showWeather);
+  axios.get(apiURL).then(displayTemperature);
 }
 
-function showWeather(response) {
-  console.log(response.data.name);
-  console.log(response.data.main.temp);
-  let city = document.querySelector("#Today");
-  city.innerHTML = response.data.name;
-  let forecastToday = document.querySelector("#forecastToday");
-  forecastToday.innerHTML = `It is ${response.data.main.temp}°C`;
-}
-
-function getCurrentCity(event) {
+function handleSubmit(event) {
   event.preventDefault();
-  navigator.geolocation.getCurrentPosition(findPosition);
+  let cityInputElement = document.querySelector("#city-input");
+  searchCity(cityInputElement.value);
 }
 
-let currentCity = document.querySelector(".getCurrent");
-currentCity.addEventListener("click", getCurrentCity);
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("Zoerl-Parwijs");
