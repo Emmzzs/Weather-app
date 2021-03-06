8ad9e98c281482415de154dc62b731659583d5b4
Emmzzs <78304688+Emmzzs@users.noreply.github.com>
2021-03-06 13:34:14 +0100

    Changed start city

--- src/app.js
+++ src/app.js
@@ -156,4 +156,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-searchCity("Tokyo")
+searchCity("Zoerle-Parwijs")
